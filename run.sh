f8f2c1b4e99c9b30bc8e588c7f1c98a7434952a6
KLB <k.besser@tu-bs.de>
2022-06-23 10:50:38 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -7,5 +7,14 @@
 # Copyright (C) 20XX ...
 # License: GPLv3
 
-echo "Figure 1: ..."
-python3 main.py --plot
+echo "Combination 1: K=3, N=10"
+python3 frequency_assignment.py -v -u 3 -f 10
+
+echo "Combination 2: K=3, N=100"
+python3 frequency_assignment.py -v -u 3 -f 100
+
+echo "Combination 1: K=10, N=100"
+python3 frequency_assignment.py -v -u 10 -f 100
+
+echo "Combination 1: K=20, N=100"
+python3 frequency_assignment.py -v -u 20 -f 100
