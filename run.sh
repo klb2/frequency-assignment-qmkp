12d8a92bd2ca7171bb2796969517793702c65b02
KLB <k.besser@tu-bs.de>
2022-06-28 12:01:51 +0200

    Update configurations of numerical example

--- run.sh
+++ run.sh
@@ -13,8 +13,14 @@ python3 frequency_assignment.py -v -u 3 -f 10
 echo "Combination 2: K=3, N=100"
 python3 frequency_assignment.py -v -u 3 -f 100
 
-echo "Combination 1: K=10, N=100"
+echo "Combination 3: K=10, N=100"
 python3 frequency_assignment.py -v -u 10 -f 100
 
-echo "Combination 1: K=20, N=100"
+echo "Combination 4: K=20, N=50"
+python3 frequency_assignment.py -v -u 20 -f 50
+
+echo "Combination 5: K=20, N=100"
 python3 frequency_assignment.py -v -u 20 -f 100
+
+echo "Combination 6: K=45, N=100"
+python3 frequency_assignment.py -v -u 45 -f 100
